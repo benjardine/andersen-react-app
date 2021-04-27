b3825fbbf8819c2adbeb96955d0fae0981cfc90f
Ben Jardine <jardine_199@hotmail.co.uk>
2021-04-27 18:45:51 +0100

    add form component

---
+++ src/Form.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+  render() {
+    return (
+      <>
+
+        <form>
+          <h1>Request a Quote</h1>
+          <h6>Dignissimos minus pariatur, tempore doloremque velit, aspernatur!</h6>
+
+        </form>
+        </>
+    );
+  }
+}
+
+export default Form; // Don’t forget to use export default!
--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,27 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Form from './Form';
+
+class Home extends Component {
+
+    constructor(props) {
+       super(props);
+
+       this.state = {
+          header: "my name is Ben"
+        }
+
+    }
+
+  render() {
+    return (
+      <div style={{backgroundColor: "lightblue", width: "100%", height: "1000px", padding: "100px"}}>
+      <Form myHeader={this.state.header}/>
+
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Home;
